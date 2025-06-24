5f46a79f571aacd975494ee91930037357e07deb
lauriszz123 <lauriszz12313@gmail.com>
2025-06-24 07:08:07 +0300

    Added some flair

--- src/plugins/termScreen.lua
+++ src/plugins/termScreen.lua
@@ -2,18 +2,33 @@
 local TerminalScreen = {}
 
 function TerminalScreen:initialize()
-	self.scale = 5
+	self.scale = 4
+	self.width = 240
+	self.height = 180
+
 	self.currentX = 1
 	self.currentY = 1
-	love.window.setMode(240 * self.scale, 180 * self.scale)
-
 	self.lines = { "" }
+
+	self.doUpdate = false
+
+	self.canvas = love.graphics.newCanvas(self.width, self.height)
+	love.window.setMode(self.width * self.scale, self.height * self.scale)
+	self.canvas:setFilter("nearest", "nearest")
+	love.graphics.setFont(love.graphics.newFont("src/plugins/pico.ttf", 5))
 end
 
 function TerminalScreen:draw()
-	for i, line in ipairs(self.lines) do
-		love.graphics.print(line, 1, (i - 1) * 16)
+	if self.doUpdate then
+		love.graphics.setCanvas(self.canvas)
+		for i, line in ipairs(self.lines) do
+			love.graphics.print(line, 0, (i - 1) * 6)
+		end
+		love.graphics.setCanvas()
 	end
+
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.draw(self.canvas, 0, 0, 0, self.scale, self.scale)
 end
 
 function TerminalScreen:read(addr, value)
@@ -21,9 +36,12 @@ function TerminalScreen:read(addr, value)
 		if value == ("\n"):byte() then
 			self.currentY = self.currentY + 1
 			self.lines[self.currentY] = ""
+			self.currentX = 1
 		else
 			self.lines[self.currentY] = self.lines[self.currentY] .. string.char(value)
+			self.currentX = self.currentX + 1
 		end
+		self.doUpdate = true
 	end
 end
 
